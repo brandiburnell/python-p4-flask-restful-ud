36b71e785f7643a8f8ea968ffd83fda775c280a9
Brandi Burnell <brandiburnell6@gmail.com>
2024-04-17 09:54:37 -0600

    complete

--- server/app.py
+++ server/app.py
@@ -79,6 +79,29 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self, id):
+
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(newsletter, attr, request.form[attr])
+
+        db.session.add(newsletter)
+        db.session.commit()
+
+        return make_response(newsletter.to_dict(), 200)
+    
+    def delete(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+
+        db.session.delete(newsletter)
+        db.session.commit()
+
+        body = {
+            "message": "record successfully deleted"
+        }
+
+        return make_response(body, 200)
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
